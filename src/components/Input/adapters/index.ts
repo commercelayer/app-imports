9d428599c04a3a19141fff99622776421fdb4ad5
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-07-25 12:59:49 +0200

    add missing adapters

--- src/components/Input/adapters/index.ts
+++ src/components/Input/adapters/index.ts
@@ -7,6 +7,12 @@ export const adapters: Record<AllowedResourceType, (schema: ZodSchema[]) => Impo
   sku_lists: (parsedCsv: ZodSchema[]) => fromCsvSchemaToImportInputCreate(parsedCsv, "sku_lists"),
   prices: (parsedCsv: ZodSchema[]) => fromCsvSchemaToImportInputCreate(parsedCsv, "prices"),
   coupons: (parsedCsv: ZodSchema[]) => fromCsvSchemaToImportInputCreate(parsedCsv, "coupons"),
+  gift_cards: (parsedCsv: ZodSchema[]) => fromCsvSchemaToImportInputCreate(parsedCsv, "gift_cards"),
+  customers: (parsedCsv: ZodSchema[]) => fromCsvSchemaToImportInputCreate(parsedCsv, "customers"),
+  customer_subscriptions: (parsedCsv: ZodSchema[]) =>
+    fromCsvSchemaToImportInputCreate(parsedCsv, "customer_subscriptions"),
+  tax_categories: (parsedCsv: ZodSchema[]) => fromCsvSchemaToImportInputCreate(parsedCsv, "tax_categories"),
+  stock_items: (parsedCsv: ZodSchema[]) => fromCsvSchemaToImportInputCreate(parsedCsv, "stock_items"),
 }
 
 const fromCsvSchemaToImportInputCreate = (parsedCsv: ZodSchema[], resourceType: AllowedResourceType): ImportCreate => {
