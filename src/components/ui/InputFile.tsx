acf2cba9ca029700bfbd0bc652c4990eeda0fab8
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-10-26 15:44:17 +0200

    add new InputFile component

---
+++ src/components/ui/InputFile.tsx
@@ -0,0 +1,55 @@
+import cn from 'classnames'
+import { UploadSimple } from 'phosphor-react'
+import invariant from 'ts-invariant'
+
+interface Props
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  /**
+   * If defined, it shows a progress bar at the bottom. It must be between 0 and 100.
+   */
+  progress?: number
+  /**
+   * Text to be shown in the file drop area. Example: "Select an image to upload"
+   */
+  label: string
+}
+
+export function InputFile({
+  className,
+  progress,
+  label,
+  ...rest
+}: Props): JSX.Element {
+  invariant(
+    progress === undefined || (progress >= 0 && progress <= 100),
+    'When set, progress must be between 0 and 100 range'
+  )
+  return (
+    <div
+      className={cn(
+        'h-52 w-full relative border border-gray-200 bg-white rounded-md flex flex-col justify-center items-center hover:bg-gray-50 transition-bg group overflow-hidden',
+        className
+      )}
+    >
+      <input
+        type='file'
+        {...rest}
+        className='inset-0 absolute opacity-0 z-10 cursor-pointer'
+      />
+      <UploadSimple className='text-3xl mb-2' />
+      <div className='font-medium'>{label}</div>
+      <div className='text-sm'>
+        drag and drop it here or{' '}
+        <span className='text-primary font-medium group-hover:underline'>
+          browse files
+        </span>
+      </div>
+      {progress != null ? (
+        <div
+          className='absolute bg-primary h-1 bottom-0 left-0 transition-all'
+          style={{ width: `${progress}%` }}
+        />
+      ) : null}
+    </div>
+  )
+}
--- src/utils/tests.ts
+++ src/utils/tests.ts
@@ -4,6 +4,9 @@ export function testInvariant(
 ): void {
   try {
     testFn()
+    // This helpers is intended to test the error massage. So it must always throw an error.
+    // If `testFn()` does not trigger any error our invariant test will not run.
+    throw Error('It works you should not have this when test invariants')
   } catch (e) {
     expect((e as any).toString()).toBe(
       `Invariant Violation: ${expectedInvariant}`
