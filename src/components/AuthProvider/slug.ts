a0669af5c27d93267710fe83c9dc28f9041a1330
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2022-11-11 11:12:28 +0100

    remove old AuthProvider and ResourceSelector components

--- src/components/AuthProvider/slug.ts
+++
@@ -1,7 +0,0 @@
-export function getOrgSlugFromCurrentUrl(): string {
-  return window.location.hostname.split('.')[0]
-}
-
-export function makeDashboardUrl(): string {
-  return `https://dashboard.commercelayer.io/${getOrgSlugFromCurrentUrl()}`
-}
