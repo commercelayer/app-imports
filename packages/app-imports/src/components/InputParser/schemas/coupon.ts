1b7d868519566ccb8fe620fd0a2d39a0d6716ef5
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-03-17 17:39:57 +0100

    fix: set coupon's `usage_limit` as optional

--- packages/app-imports/src/components/InputParser/schemas/coupon.ts
+++ packages/app-imports/src/components/InputParser/schemas/coupon.ts
@@ -1,9 +1,12 @@
 import { CouponCreate } from '@commercelayer/sdk'
 import { z } from 'zod'
-
+import { SetOptional } from 'type-fest'
 import { zodEnforceInt, zodEnforceBoolean } from './zodUtils'
 
-type FlatCsvRow = Omit<CouponCreate, 'promotion_rule'> & {
+type FlatCsvRow = Omit<
+  SetOptional<CouponCreate, 'usage_limit'>,
+  'promotion_rule'
+> & {
   promotion_rule_id?: string
 }
 
@@ -11,7 +14,7 @@ const schema = z
   .object({
     code: z.string().min(8),
     promotion_rule_id: z.optional(z.string().min(1)),
-    usage_limit: zodEnforceInt,
+    usage_limit: z.optional(zodEnforceInt),
     customer_single_use: zodEnforceBoolean({ optional: true }),
     recipient_email: z.optional(z.string()),
     reference: z.optional(z.string()),
