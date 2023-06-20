f0a886f96a8183f5c3479397de3f6364da2f9b44
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-06-20 15:06:00 +0200

    fix: allow zero quantity in stock_items

--- packages/app-imports/src/components/InputParser/schemas/stockItems.ts
+++ packages/app-imports/src/components/InputParser/schemas/stockItems.ts
@@ -2,13 +2,13 @@
 import { isFalsy } from '#utils/isFalsy'
 import { z } from 'zod'
 
-import { zodEnforcePositiveInt } from './zodUtils'
+import { zodEnforceNonNegativeInt } from './zodUtils'
 
 const makeSchema = (hasParentResourceId: boolean) =>
   z
     .object({
       sku_code: z.optional(z.string().min(1)),
-      quantity: zodEnforcePositiveInt,
+      quantity: zodEnforceNonNegativeInt,
       reference: z.optional(z.string()),
       reference_origin: z.optional(z.string()),
       stock_location_id: z.optional(z.string().min(1)),
