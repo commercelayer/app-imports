144f96dea05bbe6c68ba7ead3b67dcaa1d4ea817
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2024-02-07 15:30:00 +0100

    fix: remove mocks (msw)

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import '@commercelayer/app-elements/style.css'
 
-// init mswjs mock service worker
-if (import.meta.env.PUBLIC_ENABLE_MOCKS === 'true' && import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser')
-  void worker.start({
-    onUnhandledRequest: 'bypass'
-  })
-}
-
 // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
