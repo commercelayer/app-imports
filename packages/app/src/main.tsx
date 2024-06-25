67713586ba2fd7384bc9db53033a5670cd3d4fa6
Marco Montalbano <marco.montalbano@commercelayer.io>
2024-06-25 18:05:44 +0200

    chore: export 'Main' component as default

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -3,7 +3,8 @@ import {
   ErrorBoundary,
   MetaTags,
   TokenProvider,
-  createApp
+  createApp,
+  type ClAppProps
 } from '@commercelayer/app-elements'
 import '@commercelayer/app-elements/style.css'
 import { StrictMode } from 'react'
@@ -11,25 +12,26 @@ import { App } from './App'
 
 const isDev = Boolean(import.meta.env.DEV)
 
-createApp(
-  (props) => (
-    <StrictMode>
-      <ErrorBoundary hasContainer>
-        <TokenProvider
-          kind='imports'
-          appSlug='imports'
-          devMode={isDev}
-          reauthenticateOnInvalidAuth={!isDev && props?.onInvalidAuth == null}
-          loadingElement={<div />}
-          {...props}
-        >
-          <CoreSdkProvider>
-            <MetaTags />
-            <App routerBase={props?.routerBase} />
-          </CoreSdkProvider>
-        </TokenProvider>
-      </ErrorBoundary>
-    </StrictMode>
-  ),
-  'imports'
+const Main: React.FC<ClAppProps> = (props) => (
+  <StrictMode>
+    <ErrorBoundary hasContainer>
+      <TokenProvider
+        kind='imports'
+        appSlug='imports'
+        devMode={isDev}
+        reauthenticateOnInvalidAuth={!isDev && props?.onInvalidAuth == null}
+        loadingElement={<div />}
+        {...props}
+      >
+        <CoreSdkProvider>
+          <MetaTags />
+          <App routerBase={props?.routerBase} />
+        </CoreSdkProvider>
+      </TokenProvider>
+    </ErrorBoundary>
+  </StrictMode>
 )
+
+export default Main
+
+createApp(Main, 'imports')
